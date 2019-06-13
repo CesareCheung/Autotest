2647618bcfe9fe0c9d2a8cdcbd0f63010b9158f8
zhangweixu <1689719986@qq.com>
2019-06-13 14:14:32 +0800

    PO页面元素封装

--- Page_Object/Common_Page/login_page.py
+++ Page_Object/Common_Page/login_page.py
@@ -3,20 +3,19 @@ from selenium.webdriver.common.by import By
 from Common.selenium_library import SeleniumBase
 
 
-
 class Login_Page(SeleniumBase):
-    """首页页面对象"""
-    loc_user=(By.ID,"username")
-    loc_psw=(By.ID,"password")
-    loc_sub=(By.ID,"submit")
+    """登录页面对象"""
+    loc_user = (By.ID, "username")
+    loc_psw = (By.ID, "password")
+    loc_sub = (By.ID, "submit")
 
-    def send_username(self,username):
+    def send_username(self, username):
         self.logger.info(f"输入用户名{username}")
-        self.send_keys(self.loc_user,username)
+        self.send_keys(self.loc_user, username)
 
-    def send_password(self,password):
+    def send_password(self, password):
         self.logger.info(f"输入密码{password}")
-        self.send_keys(self.loc_psw,password)
+        self.send_keys(self.loc_psw, password)
 
     def submit(self):
         self.logger.info("点击登录按钮")
