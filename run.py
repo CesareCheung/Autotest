7d75fc96b385307f0b946ce91912005585a60191
zhangweixu <1689719986@qq.com>
2019-06-13 14:15:27 +0800

    脚本运行文件

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# 运行脚本
+# 脚本运行文件
 import unittest
 from TestSuite.LinesSuite import lines_suite
 from TestSuite.LinesSuite import itinerary_suite
@@ -14,6 +14,6 @@ suite.addTests(lines_suite.return_suite())
 HTMLReport.TestRunner(
     report_file_name="test",
     title='WEB UI 自动化',
-    description='小强ERP项目',
+    description='ERP项目',
     thread_count=3
 ).run(suite)
